5be5a07fa91c972742b9a81506e0164852d3547f
mircowuffwuff <sleepymirco@gmail.com>
2026-07-24 18:30:43 +0200

    fix RomVerifier scene, implement PackedScene loading in Wrapper and bump version to 9.1

--- Scripts/Wrapper.gd
+++ Scripts/Wrapper.gd
@@ -9,19 +9,27 @@ func _ready() -> void:
 	await get_tree().process_frame
 	await change_scene_to("res://Scenes/Levels/Disclaimer.tscn")
 
-func change_scene_to(path) -> void:
+func change_scene_to(scene_path) -> void:
 	for child in game_viewport.get_children():
 		if child.name != "Global":
 			child.queue_free()
 			await child.tree_exited
 	
-	# TODO add packed scene support, similar to:
 	#if scene_path is String:
 	#	get_tree().change_scene_to_file(scene_path)
 	#elif scene_path is PackedScene:
 	#	get_tree().change_scene_to_packed(scene_path)
 	
-	var new_scene = load(path).instantiate()
+	var new_scene
+	if scene_path is String:
+		print("----")
+		print(scene_path)
+		print(load(scene_path))
+		print(load(scene_path).instantiate())
+		print("----")
+		new_scene = load(scene_path).instantiate()
+	elif scene_path is PackedScene:
+		new_scene = scene_path.instantiate()
 	game_viewport.add_child(new_scene)
 	
 	await new_scene.ready
